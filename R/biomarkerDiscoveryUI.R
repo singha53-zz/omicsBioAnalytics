f0ab9807422e8a3f8c4fdcae73d678b114610bb9
Amrit Singh <asingh.analytics@gmail.com>
2020-03-30 13:10:39 -0400

    add validation to biomarker discovery analysis

--- R/biomarkerDiscoveryUI.R
+++ R/biomarkerDiscoveryUI.R
@@ -10,7 +10,8 @@ biomarkerDiscoveryUI <- function() {
             tags$style(type="text/css", ".modelInput label{ display: table-cell; } .modelInput .form-group { display: table-row;}"),
 
             # Input: Slider for the number of observations to generate ----
-            h4("Model parameters:"),
+            h4("Setting:"),
+            checkboxGroupInput("selectedGroups", "Select two groups to compare (required):", "", "", inline = TRUE),
             sliderInput("alpha",
               "Variable selection (alpha values)",
               min = 0, max = 1, value = c(0.7, 1)),
@@ -49,12 +50,14 @@ biomarkerDiscoveryUI <- function() {
             # Input: Number of iterations
             tags$div(class = "modelInput", numericInput(inputId="n_repeats", h5("Number of times to repeat CV:", style = 'padding:0px 6px 0px 0px;'), value = 5, width = "50%")),
 
-            actionButton("build", "Build Penalized Regression Models!", icon = icon("power-off"), style="color: #fff; background-color: #337ab7; border-color: #2e6da4", width = "100%")),
+            actionButton("build", "Build Penalized Regression Models!", icon = icon("power-off"), style="color: #fff; background-color: #337ab7; border-color: #2e6da4", width = "100%")
+            ),
 
           mainPanel(
 
             tabsetPanel(type = "tabs",
               tabPanel("Performance",
+                uiOutput("errMsg"),
                 fluidRow(
                   column(6,
                     tags$div(plotlyOutput("rocPlot"),
@@ -65,10 +68,7 @@ biomarkerDiscoveryUI <- function() {
                       h4("Optimal biomarker panels", align = "center"),
                       DT::dataTableOutput("aucs"), style = 'padding:100px 100px 0px 0px;')
                   )
-                ),
-                fluidRow(column(2, ""), column(8, h3("Compare biomarker panels"), plotOutput("delongPvalues")), column(2, "")),
-                fluidRow(column(2, ""), column(8, h5("*P-value< 0.05. Predicted probablities across all iterations were averaged for each subject prior to performing the De Long test.")), column(2, ""))
-              ),
+                )),
               tabPanel("Panels",
                 fluidRow(column(2, ""), column(8, h4("Overlap between single and ensemble biomarker panels"), plotOutput("panelN"),
                   downloadButton("biomarkerPanels", label = HTML("<span style='font-size:1em;'>Download<br/>Biomarkers</span>"), style="color: #fff; background-color: #FF7F00; border-color: #2e6da4")
