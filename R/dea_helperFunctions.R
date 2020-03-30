9c4bb73d965ed7c2a2905195ad57c29439ff85b7
Amrit Singh <asingh.analytics@gmail.com>
2020-03-30 06:34:28 -0400

    add generateTopTable function

--- R/dea_helperFunctions.R
+++ R/dea_helperFunctions.R
@@ -4,3 +4,31 @@ jaccard <- function (s1, s2) {
   length(intersect(s1, s2))/length(union(s1, s2))
 }
 
+#' @export
+#' @rdname generateTopTable
+generateTopTable = function(eset, design, coefNumber, test){
+  if (test == "limma"){
+    fit <- eBayes(lmFit(t(eset), design))
+    top <- topTable(fit, coef = coefNumber, adjust.method = "BH", n = nrow(fit), sort.by="none")
+    top %>% mutate(FeatureName = colnames(eset),
+      sig = -log10(P.Value)) %>%
+      arrange(P.Value)
+  } else if (test == "vlimma") {
+    v <- voom(2^t(eset), design, plot=TRUE)
+    fit <- lmFit(v, design)
+    fit <- eBayes(fit)
+    top <- topTable(fit, coef = coefNumber, n = nrow(fit), adjust.method = "BH")
+    top %>% mutate(FeatureName = colnames(eset),
+      sig = -log10(P.Value)) %>%
+      arrange(P.Value)
+  } else {
+    # assume OLS
+    fit <- lmFit(t(eset), design)
+    fit$t <- fit$coef/fit$stdev.unscaled/fit$sigma
+    fit$p.value <- 2 * pt(-abs(fit$t), df = fit$df.residual)
+    top <- topTable(fit, coef = coefNumber, adjust.method = "BH", n = nrow(fit), sort.by="none")
+    top %>% mutate(FeatureName = colnames(eset),
+      sig = -log10(P.Value)) %>%
+      arrange(P.Value)
+  }
+}
