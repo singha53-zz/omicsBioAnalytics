a094a399eab4da6fcd149ebbeb38336391c2c4a6
Amrit Singh <asingh.analytics@gmail.com>
2020-04-03 17:03:36 -0400

    add list of methods/tools

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -96,7 +96,24 @@ body <- dashboardBody(
         ),
         tabItem(
           tabName = "methods",
-          "methods"
+          h5("R version 3.6.1"),
+          h5("RStudio version 1.2.5019."),
+          fluidRow(column(6, h3("Analyses")),
+            column(6, h3("R-packages"))),
+          fluidRow(column(6, h5("Development of web application")),
+            column(6, h5("shiny (v1.1), shinydashboard (v0.7.1), shinyBS (0.61)"))),
+          fluidRow(column(6, h5("Data wrangling")),
+            column(6, h5("tidyverse (v1.2.1)"))),
+          fluidRow(column(6, h5("Exploratory Data Analysis")),
+            column(6, h5("stats (v3.5.1)"))),
+          fluidRow(column(6, h5("Differential expression analysis")),
+            column(6, h5("limma (v3.40.6)"))),
+          fluidRow(column(6, h5("Biomarker analysis")),
+            column(6, h5("caret (v6.0-84), glmnet (v2.0-18), pROC (v1.15.3)"))),
+          fluidRow(column(6, h5("Pathway enrichment analysis")),
+            column(6, h5("enrichR (v2.1)"))),
+          fluidRow(column(6, h5("graphical visualizations")),
+            column(6, h5("plotly (v4.9.1), d3heatmap (v0.6.1.2), RColorBrewer (v1.1-2), visNetwork (v2.0.9), googleVis (v0.6.4), igraph (v1.2.4.1), canvasXpress (v1.26.5), DT (v0.11), UpSetR (v1.4.0)")))
         ),
         tabItem(
           tabName = "report",
