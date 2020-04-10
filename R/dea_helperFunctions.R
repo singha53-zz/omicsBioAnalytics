b67a3ae59bbcc1b146e0c14d0a3fb407c452f977
Amrit Singh <asingh.analytics@gmail.com>
2020-04-10 14:00:35 -0400

    feat: displayed results of geneset enrichment analysis using heatmaps
    * removed networks that displayed up- and down-regulated features (too noisy)
    * add heatmaps to display enriched pathways and gene members colored by regulation-type
    * add reactive sliders to control number of pathways/compounds to display
    
    resolves: #28

--- R/dea_helperFunctions.R
+++ R/dea_helperFunctions.R
@@ -32,3 +32,42 @@ generateTopTable = function(eset, design, coefNumber, test){
       arrange(P.Value)
   }
 }
+
+
+#' @export
+#' @rdname drugHeatmap
+drugHeatmap = function(fc, genesetList, col, datasetName, GeneSetName){
+  ## make data frame for ggplot
+  Dat <- data.frame(Gene = unlist(genesetList),
+    Pathway = rep(names(genesetList), unlist(lapply(genesetList, length))),
+    FC = fc[unlist(genesetList)])
+
+  Dat$FC[Dat$FC > 0] <- "Up";
+  Dat$FC[Dat$FC == 0] <- "NoChange";
+  Dat$FC[Dat$FC < 0] <- "Down"
+  Dat$FC <- factor(Dat$FC, levels = c("Up", "NoChange", "Down"))
+  ## order genes and pathways based on there popularity (number of times they repeat)
+  Dat$Gene <- factor(as.character(Dat$Gene), levels = names(table(as.character(Dat$Gene))[order(table(as.character(Dat$Gene)))]))
+  Dat$Pathway <- factor(as.character(Dat$Pathway), levels = names(table(as.character(Dat$Pathway))[order(table(as.character(Dat$Pathway)))]))
+  Dat$Pathway <- unlist(lapply(strsplit(as.character(Dat$Pathway), " "), function(i){
+    lb <- paste(c(i[1:3], "\n", i[4:6], "\n", i[7:10]), collapse = " ")
+    gsub("NA", "", lb)
+  }))
+
+
+  if(length(table(Dat$FC)) < 2){
+    stop(paste("Please specify", length(table(Dat$FC)), "colors", sep = "_"))
+  }
+
+  ## Signficant Pathway Map
+  ggplot(Dat, aes(x = Gene, y = Pathway, fill = FC)) + geom_tile() +
+    geom_rect(aes(xmin = -Inf,
+      xmax = Inf, ymin = -Inf, ymax = Inf), fill = "black",
+      alpha = 0.03) +
+    xlab(datasetName) + scale_fill_manual(values=col) +
+    geom_tile() +
+    customTheme(sizeStripFont = 10, xAngle = 90, hjust = 1, vjust = 1, xSize = 5,
+      ySize = 5, xAxisSize = 8, yAxisSize = 8) +
+    theme(panel.background = element_rect(fill = "black")) +
+    ylab(GeneSetName)
+}
