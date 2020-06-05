3cead609dddf3e19da69991ad8f2cce2e16925d6
Amrit Singh <asingh@Amrits-MacBook-Pro.local>
2020-06-05 18:23:27 -0400

    change appDir to run shiny app

--- R/startApp.R
+++ R/startApp.R
@@ -3,8 +3,7 @@
 #' @param ... passed to \link[shiny]{runApp}
 #' @export
 #' @rdname startApplication
-#' from https://github.com/sk-sahu/sig-bio-shiny/blob/master/R/run.R
 start_app <- function() {
-  appDir <- system.file("inst/app", package = "omicsBioAnalytics")
+  appDir <- system.file("/app", package = "omicsBioAnalytics")
   shiny::runApp(appDir, launch.browser = TRUE)
 }
