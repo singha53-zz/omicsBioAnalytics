a26b2e5642d20607dba4a5017fc5079908553908
Amrit Singh <asingh@Amrits-MacBook-Pro.local>
2020-06-05 17:53:48 -0400

    bug: fixed start_app() *fixed manual \emphasis errors *added namespaces to package functions
    resovles: #57

--- R/startApp.R
+++ R/startApp.R
@@ -3,6 +3,8 @@
 #' @param ... passed to \link[shiny]{runApp}
 #' @export
 #' @rdname startApplication
+#' from https://github.com/sk-sahu/sig-bio-shiny/blob/master/R/run.R
 start_app <- function() {
-  shiny::runApp("inst/app")
+  appDir <- system.file("inst/app", package = "omicsBioAnalytics")
+  shiny::runApp(appDir, launch.browser = TRUE)
 }
